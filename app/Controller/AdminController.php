1dd0a76913f51a0221a5ca43994d001bad24fc43
Malith Wijethunga <mwmalith@gmail.com>
2014-10-18 17:16:19 +0530

    add main category

---
+++ app/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppController', 'Controller');
+
+/**
+ * CakePHP AdminController
+ * @author Malith_pc
+ */
+class AdminController extends AppController {
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+    
+    function login() {
+        
+    }
+
+}
